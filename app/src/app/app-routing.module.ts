596b4c004b21e7ae05eb288e1e9a096a116027ae
norma veronica leaño <verito.cba933@gmail.com>
2020-08-31 18:06:03 -0300

    Implementación de rutas, para las pages

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
+import { DefaultComponent } from './components/default/default.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'movies', component: MoviesComponent },
+  { path: 'about', component: DefaultComponent },
+  { path: 'contact', component: DefaultComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
