6fe79554c2076133da3cf17693860d886ef19e65
norma veronica leaño <verito.cba933@gmail.com>
2020-08-31 18:08:15 -0300

    Implementaciones y cambios

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,16 +7,28 @@ import { MenuComponent } from './components/menu/menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { MoviesComponent } from './components/movies/movies.component';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { MatSliderModule } from '@angular/material/slider';
+import { DefaultComponent } from './components/default/default.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    MoviesComponent
+    MoviesComponent,
+    DefaultComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
