2273a6162ceb048227b1929f99dee4d259da468c
norma veronica leaño <verito.cba933@gmail.com>
2020-08-31 15:33:16 -0300

    Cambios en los siguientes files: component.html, app.module.ts

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
